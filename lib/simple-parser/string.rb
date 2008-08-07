48e0b7b16e73ea42cc506c6e53402a72c19640b9
David Dollar <ddollar@gmail.com>
2008-08-07 14:28:16 -0400

    added token for things we dont care about

--- lib/simple-parser/string.rb
+++ lib/simple-parser/string.rb
@@ -1,16 +1,16 @@
 module SimpleParser::String
-  
+
   def self.included(base)
     base.send(:define_method, :parse) do |format|
       SimpleParser::String::parse(self, format)
     end
   end
-  
+
   @parser_cache = {}
-  
+
   def self.parse(string, format)
     parser = @parser_cache[format] || SimpleParser::String::build_parser(format)
-    
+
     matches = string.match(parser[:regexp]).to_a
     matches.shift
 
@@ -19,21 +19,23 @@ module SimpleParser::String
       memo
     end
   end
-  
+
   private
-  
+
   def self.build_parser(format)
-    tokens = format.scan(/\:\w*/).map { |token| token[1..-1] }
+    tokens = format.scan(/\:\w+/).map { |token| token[1..-1] }
 
     regexp_builder = "^#{Regexp.escape(format)}$"
 
     tokens.each do |token|
-      regexp_builder.gsub!(":#{token}", '(.*?)')
+      regexp_builder.gsub!(":#{token}", '(.+?)')
     end
-  
+
+    regexp_builder.gsub!('::', '.+?')
+
     regexp = Regexp.new(regexp_builder)
-    
+
     @parser_cache[format] = { :tokens => tokens, :regexp => regexp }
   end
-  
+
 end
--- spec/simple-parser_string_spec.rb
+++ spec/simple-parser_string_spec.rb
@@ -3,19 +3,19 @@ require 'simple-parser'
 include SimpleParser
 
 describe SimpleParser::String do
-  
+
   describe 'rspec' do
 
     it "should respond to parse" do
       SimpleParser::String.should respond_to(:parse)
     end
-    
+
     it "should parse a basic string" do
       SimpleParser::String::parse('this test', ':word1 :word2').should ==(
         { :word1 => 'this', :word2 => 'test' }
       )
     end
-    
+
     it "should parse a complex string" do
       SimpleParser::String.parse(
         '127.0.0.1 [12/May/2007 00:00:00] "Test Data Here" - - Foo ',
@@ -29,7 +29,7 @@ describe SimpleParser::String do
         :attr3       => 'Foo'
       })
     end
-    
+
     it "should be includable on strings" do
       class String; include SimpleParser::String; end
 
@@ -38,6 +38,12 @@ describe SimpleParser::String do
       )
     end
 
+    it "should be able to ignore tokens" do
+      SimpleParser::String.parse('this bad test', ':word1 :: :word2').should == (
+        { :word1 => 'this', :word2 => 'test' }
+      )
+    end
+
   end
-  
+
 end
